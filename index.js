fafe4e0dd9f93ab9c878b380dffa68b60bee1364
Anthony Curtis <tonydc1997@gmail.com>
2018-08-21 10:04:06 -0700

    Fix minor error

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 import React from 'react';
-import ReactDom from 'react-dom';
+import ReactDOM from 'react-dom';
 
 const Hello = () => <div>Hello World!</div>;
+
+ReactDOM.render(<Hello />, document.getElementById('app'));
